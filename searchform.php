8a77453ea5ac00a3d4c70a88dd3ede8b9c6d31a9
Igor Scheller <igor.scheller@igorshp.de>
2014-11-14 19:51:48 +0100

    Suchformular - Stylefix

--- searchform.php
+++ searchform.php
@@ -7,15 +7,14 @@
 
 
 
-<form action="/" method="get" class="form-inline" role="form">
-
-    <div class="form-group">
-        <label class="sr-only" for="search">Suche</label>
-        <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="<?php echo __('Search', 'bicbswp') ?>" class="form-control input-sm"/>
+<form action="/" method="GET" id="s" role="search">
+    <div class="input-group">
+        <label class="sr-only" for="search"><?php echo __('Search', 'bicbswp') ?></label>
+        <input type="text" class="form-control" id="search" name="s" placeholder="<?php echo __('Search', 'bicbswp') ?>" value="<?php the_search_query(); ?>">
+        <div class="input-group-btn">
+            <button type="submit" class="btn btn-default">
+                <span class="glyphicon glyphicon-search"></span>
+            </button>
+        </div>
     </div>
-
-    <div class="form-group">
-        <button type="submit" class="btn btn-default"><?php echo __('Search', 'bicbswp') ?></button>
-            </div>
-
 </form>
