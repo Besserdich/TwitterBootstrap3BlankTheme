dd895b064e9c287bf06a40db802d86be43358367
Nina Taberski-Besserdich <nina.taberski@besserdich.com>
2014-12-18 13:10:32 +0100

    removed style class „main“ from content div

--- page.php
+++ page.php
@@ -47,7 +47,7 @@
          
                 <div class="col-lg-9 col-md-9 col-sm-9 col-9">
                     
-                    <div class="content main">
+                    <div class="content">
                         
              <header>
                        
